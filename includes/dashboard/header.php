925c625d58ca1813f4ad108660298eac123941ab
Mshandete <paschalmshandete25@gmailcom>
2026-07-10 16:40:55 +0300

    Dashboard started

---
+++ includes/dashboard/header.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+$pageTitle = $pageTitle ?? "Dashboard";
+?>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?= htmlspecialchars($pageTitle) ?> | FindPro</title>
+<meta name="description" content="FindPro Local Services Finder Dashboard">
+<meta name="author" content="FindPro">
+<link rel="icon" href="../assets/images/mds.png">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+<link rel="stylesheet" href="../assets/css/dashboard.css">
